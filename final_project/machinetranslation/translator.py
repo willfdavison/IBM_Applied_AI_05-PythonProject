0f643a1e107e8bb2fab10af6aef30c6efbe8f75c
willfdavison <will.f.davison@gmail.com>
2022-05-10 14:26:39 +0000

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -10,3 +10,23 @@ apikey = os.environ['apikey']
 url = os.environ['url']
 
 
+authenticator = IAMAuthenticator(apikey)
+translator = LanguageTranslatorV3(authenticator=authenticator)
+translator.set_service_url(url)
+
+
+def englishToFrench(englishText):
+    #Translate text
+    translation = translator.Translate(text=englishText, model_id='en-fr').get_result()
+    #Obtain string from returned list
+    frenchText = translation['translations'][0]['translation']
+    #Return translated string
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    #Translate text
+    translation = translator.Translate(text=frenchText, model_id='fr-en').get_result()
+    #Obtain string from returned list
+    englishText = translation['translations'][0]['translation']
+    #Return translated string
+    return englishText
